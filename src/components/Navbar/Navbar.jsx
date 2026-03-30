6dd7618428db0f7e66cafef4e779547d81a6b4a0
forthright47 <k.tuhinjaman@gmail.com>
2026-03-30 12:03:30 +0600

    make the navbar responsive for small devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     return (
         <div className='navbar shadow-sm items-center lg:px-25 py-2'>
             <div className='navbar-start'>
-                <h2 className='text-4xl font-bold bg-linear-to-br from-violet-800 to-violet-500 bg-clip-text text-transparent py-2'>DigiTools</h2>
+                <h2 className='lg:text-4xl text-3xl font-bold bg-linear-to-br from-violet-800 to-violet-500 bg-clip-text text-transparent py-2'>DigiTools</h2>
             </div>
-            <div className='navbar-center'>
+            <div className='navbar-center hidden lg:flex'>
                 <ul className='flex items-center gap-7 text-black/80'>
                     <li>
                         <a href="">Products</a>
@@ -27,9 +27,9 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className='navbar-end flex items-center gap-5'>
-                <ShoppingCart />
-                <p className='font-medium'>Login</p>
-                <button className='bg-linear-to-r from-violet-800 to-violet-500 text-white font-semibold py-3 px-5 rounded-full'>Get Started</button>
+                <ShoppingCart className='hidden lg;flex'/>
+                <p className='font-medium hidden lg:flex'>Login</p>
+                <button className='bg-linear-to-r from-violet-800 to-violet-500 text-white lg:font-semibold font-medium lg:py-3 lg:px-5 p-2.5 rounded-full lg:text-lg text-sm'>Get Started</button>
             </div>
         </div>
     );
