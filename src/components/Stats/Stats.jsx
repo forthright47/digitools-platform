909c92166894dbd17410998cf1b824ef49fec1bb
forthright47 <k.tuhinjaman@gmail.com>
2026-03-31 14:06:42 +0600

    fixed padding inconsistencies in the navbar and stats section

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Stats = () => {
     return (
-        <div className='flex flex-col lg:flex-row justify-center px-10 lg:px-40 py-8 bg-linear-to-br from-violet-800 to-violet-500 text-white mt-15'>
+        <div className='flex flex-col lg:flex-row justify-center px-10 lg:px-40 py-6 bg-linear-to-br from-violet-800 to-violet-500 text-white mt-15'>
             <div className='flex-1 px-5 lg:px-20 text-center py-5 border-b lg:border-b-0 lg:border-r border-white/30'>
                 <h1 className='text-4xl lg:text-5xl font-bold'>50K+</h1>
                 <p className='text-white/70 mt-2'>Active Users</p>
