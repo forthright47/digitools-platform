506d0a9e4b64e7af4b57f7d5d2cdfc5224499678
forthright47 <k.tuhinjaman@gmail.com>
2026-03-31 14:04:21 +0600

    make the stats section responsive for small devices

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Stats = () => {
     return (
-        <div className='flex justify-center px-50 py-10 bg-linear-to-br from-violet-800 to-violet-500 text-white mt-18 divide-x divide-white/30'>
-            <div className='flex-1 px-20 text-center'>
-                <h1 className='text-5xl font-bold'>50K+</h1>
+        <div className='flex flex-col lg:flex-row justify-center px-10 lg:px-40 py-8 bg-linear-to-br from-violet-800 to-violet-500 text-white mt-15'>
+            <div className='flex-1 px-5 lg:px-20 text-center py-5 border-b lg:border-b-0 lg:border-r border-white/30'>
+                <h1 className='text-4xl lg:text-5xl font-bold'>50K+</h1>
                 <p className='text-white/70 mt-2'>Active Users</p>
             </div>
-            <div className='flex-1 px-20 text-center'>
-                <h1 className='text-5xl font-bold'>200+</h1>
+            <div className='flex-1 px-5 lg:px-20 text-center py-5 border-b lg:border-b-0 lg:border-r border-white/30'>
+                <h1 className='text-4xl lg:text-5xl font-bold'>200+</h1>
                 <p className='text-white/70 mt-2'>Premium Tools</p>
             </div>
-            <div className='flex-1 px-20 text-center'>
-                <h1 className='text-5xl font-bold'>4.9</h1>
+            <div className='flex-1 px-5 lg:px-20 text-center py-5'>
+                <h1 className='text-4xl lg:text-5xl font-bold'>4.9</h1>
                 <p className='text-white/70 mt-2'>Rating</p>
             </div>
         </div>
